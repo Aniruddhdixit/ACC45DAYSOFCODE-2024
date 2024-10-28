b02ebbe330b05f5650dde962488e10271bd983bd
Aniruddhdixit <160569643+Aniruddhdixit@users.noreply.github.com>
2024-10-28 23:00:25 +0530

    Add files via upload

---
+++ Day 26-Degree_Of_Polynomial.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+int t;
+cin>>t;
+while(t--){
+    string s;
+    cin>>s;
+   sort(s.begin(),s.end());
+   int count=0;
+   for(int i=0;i<s.size();i++){
+       if(s[i]==s[i+1]){
+       count++;
+       i++;
+       }
+       else
+       count++;
+   }
+   cout<<count<<endl;
+}
+}
