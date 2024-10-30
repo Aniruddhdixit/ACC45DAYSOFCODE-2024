585f3a396e006ae3ba0aa4d3e65bf2abc9939f6e
Aniruddhdixit <160569643+Aniruddhdixit@users.noreply.github.com>
2024-10-30 21:31:11 +0530

    Add files via upload

---
+++ Day 28-Retrieve_The_Array.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+int t;
+cin>>t;
+while(t--){
+    int n;
+    cin>>n;
+    long long b[n],sumb=0,a;
+    for(int i=0;i<n;i++){
+    cin>>b[i];
+    sumb+=b[i];
+    }
+    a=sumb/(n+1);
+    for(int i=0;i<n;i++)
+    cout<<b[i]-a<<" ";
+    cout<<endl;
+}
+}
