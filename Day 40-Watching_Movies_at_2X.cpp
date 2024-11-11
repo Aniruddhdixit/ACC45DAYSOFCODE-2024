1a46b51ce66e9488b0c4de5b5238328621c31352
Aniruddhdixit <160569643+Aniruddhdixit@users.noreply.github.com>
2024-11-11 23:50:53 +0530

    Add files via upload

---
+++ Day 40-Watching_Movies_at_2X.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+   
+ 
+       
+       int x,y,save_time;
+       cin>>x>>y;
+       save_time=y/2;
+       cout<<x-save_time<<endl;
+       
+       
+       
+   
+}
